790e6f67faeb5c6fca9785d340d5a2c68b12c95c
Corey Savage <mastasav@gmail.com>
2017-05-12 10:20:27 -0400

    All functionality complete

--- oauth_demo.py
+++ oauth_demo.py
@@ -27,50 +27,59 @@ key = ''.join((random.choice(chars)) for x in range(pwdSize))
 
 secret_key = key
 
-config = {}
-config['webapp2_extras.sessions'] = {'secret_key': key,}
-
-
-
-
 jinja_environment = jinja2.Environment(
    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates')),
    autoescape = True)
 
-
-class BaseRequestHandler(webapp2.RequestHandler):
-    def dispatch(self):
-        self.session_store = sessions.get_store(request=self.request)
-
-        try:
-            # Dispatch the request.
-            webapp2.RequestHandler.dispatch(self)
-        finally:
-            # Save all sessions.
-            self.session_store.save_sessions(self.response)
-
-    @webapp2.cached_property
-    def session(self):
-        # Returns a session using the default cookie key.
-        return self.session_store.get_session()
-
-
-
-class OauthHandler(BaseRequestHandler):
+class OauthHandler(webapp2.RequestHandler):
     def get(self):
         code = self.request.get('code')
-        url = 'https://www.googleapis.com/oauth2/v4/token'
-        self.response.write(code)
         data_to_post = {
             'code': code,
             'client_id':'838080691485-jf0lut0oq1tdirerhoa9tc7lf3roard3.apps.googleusercontent.com',
             'redirect_uri':'https://oauth2demo-167110.appspot.com/oauth',
             'client_secret':'kXaXyMr8ssNNZkzpRWdzUw3-',
             'grant_type': 'authorization_code' }
-        encoded_data = urllib.urlencode(data_to_post)
         # Send encoded data to application-2
-        result = urlfetch.fetch(url, encoded_data, method='POST')
-        self.response.write(self.request.get('access_token'))
+        try:
+            form_data = urllib.urlencode(data_to_post)
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            result = urlfetch.fetch(
+                url='https://www.googleapis.com/oauth2/v4/token',
+                payload=form_data,
+                method=urlfetch.POST,
+                headers=headers)
+
+            try:
+                results_json = json.loads(result.content)
+                token = results_json['access_token']
+                authorizeString = 'Bearer ' + token
+                plus_header = {'Authorization': authorizeString}
+                user_info = urlfetch.fetch(
+                    url='https://www.googleapis.com/plus/v1/people/me',
+                    payload=None,
+                    method=urlfetch.GET,
+                    headers=plus_header)
+
+
+                results_json = json.loads(user_info.content)
+                userName = results_json['displayName']
+                userEmail = results_json['emails']
+                url='https://oauth2demo-167110.appspot.com'
+                user_data_to_post = urllib.urlencode({
+                    'displayName': userName,
+                    'email': userEmail})
+                try:
+                    address = url + '?' + user_data_to_post
+                    self.redirect(address)
+                except urllib2.URLError:
+                    logging.exception('Caught exception fetching url')
+
+            except urlfetch.Error:
+                logging.exception('Caught exception fetching url')
+
+        except urlfetch.Error:
+            logging.exception('Caught exception fetching url')
 
 
     def post(self):
@@ -83,18 +92,25 @@ class OauthHandler(BaseRequestHandler):
         self.response.write(content)
 
 
-class MainPage(BaseRequestHandler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        template_values = {
-            'a_variable':'Welcome to the web page',
-            'title':"Main",
-            'url':"https://oauth2demo-167110.appspot.com"
-        }
-        self.response.write("hello")
-        template = jinja_environment.get_template('index.html')
-        self.response.write(template.render(template_values))
+        if self.request.get('displayName'):
+            userName = self.request.get('displayName')
+            template_values = {
+                'greeting':'Welcome, ',
+                'userName':userName
+            }
+            template = jinja_environment.get_template('index.html')
+            self.response.write(template.render(template_values))
+        else:
+            template_values = {
+                'greeting':'Welcome, stranger!',
+                'guest':'True'
+            }
+            template = jinja_environment.get_template('index.html')
+            self.response.write(template.render(template_values))
 
-    def post(self):
+    def post(self, displayName=None):
         url = 'https://accounts.google.com/o/oauth2/v2/auth'
         data = urllib.urlencode({'response_type':'code',
                                 'client_id':'838080691485-jf0lut0oq1tdirerhoa9tc7lf3roard3.apps.googleusercontent.com',
@@ -113,4 +129,4 @@ class MainPage(BaseRequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/oauth', OauthHandler)
-], debug=True, config=config)
+], debug=True)
